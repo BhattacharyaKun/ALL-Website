bffc822b34007fdfe4034cde739930a6c719e27b
Prasanta Bhattacharya <PrasantaKun@users.noreply.github.com>
2020-04-14 23:15:27 +0530

    Initial commit

---
+++ js/p5js.js
@@ -0,0 +1,21 @@
+let inconsolata;
+function preload() {
+  inconsolata = loadFont('assets/Girassol-Regular.ttf');
+}
+function setup() {
+  createCanvas(100, 100, WEBGL);
+  textFont(inconsolata);
+  textSize(width / 3);
+  textAlign(CENTER, CENTER);
+}
+function draw() {
+  background(0);
+  let time = millis();
+  rotateX(time / 1000);
+  rotateZ(time / 1234);
+  text('p5.js', 0, 0);
+}
+
+function windowResized() {
+   resizeCanvas(windowWidth, windowHeight);
+}
